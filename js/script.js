d3ea6041fafc76a040f7301357e02bd2e496c806
ShinTakuson <nwonknubot@hotmail.com>
2021-11-01 20:34:53 -0700

    added console logs with my name and my favorite foods, used let variables. take-home done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let myName = "Angel Ramirez";
+let Hello = "Hello World";
+let myFavoriteFood = "Hotdogs, Pizza, and Smoked food";
+
+// my single line comment
+/* my multi-line comment
+line 2
+line 3*/
+console.log(Hello);
+console.log("My name is " + myName + ".");
+console.log("My favorite foods are " +  myFavoriteFood + "!");
