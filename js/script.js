9bc329fd7dd8d1409f1f9c7d89ac043b89e5b5e9
ShinTakuson <nwonknubot@hotmail.com>
2021-11-03 19:21:27 -0700

    Day 2 daily challenge complete

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ line 3 */
 console.log(Hello);
 console.log("My name is " + myName + ".");
 console.log("My favorite foods are " +  myFavoriteFood + "!");
+
+let Supercalifragilisticexpialidocious = "Supercalifragilisticexpialidocious";
+
+let n = 123
+const bigNumber = 1234567890123456789012345678901234567890n
+
+console.log(Supercalifragilisticexpialidocious);
+
+console.log (`The amount of letters in ${Supercalifragilisticexpialidocious} is ` + Supercalifragilisticexpialidocious.length +`!`);
+
+console.log(`My console.log number is n, `+ n + `. Its type of is ` + typeof(n) +`.`)
+
+console.log(`My console.log number is n, `+ bigNumber + `, the n is added at the end but doesn't come up on the log. Its type of is ` + typeof(bigNumber) +`.`)
